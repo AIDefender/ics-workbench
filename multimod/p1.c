e19380649bc812cca0ad03d103f2b0d7dfc5c2e6
tracer-nju <tracer@nju.edu.cn>
2019-09-23 17:51:25 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 329caf5324ad4df7b5d7c793b9af598f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  17:51:26 up 1 day,  9:24,  1 user,  load average: 2.29, 1.91, 1.54

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,21 +1,22 @@
 #include "multimod.h"
 #define ll int64_t
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-    ll res = 0; // Initialize result 
-    a = a % m; 
-    while (b > 0) 
-    { 
-        // If b is odd, add 'a' to result 
-        if (b % 2 == 1) 
-            res = (res + a) % m; 
-  
-        // Multiply 'a' with 2 
-        a = (a * 2) % m; 
-  
-        // Divide b by 2 
-        b /= 2; 
-    } 
-  
-    // Return result 
-    return res % m;
+  ll res = 0; // Initialize result 
+  a = a % m; 
+  printf("%d\n",a);
+  while (b > 0) 
+  { 
+      // If b is odd, add 'a' to result 
+      if (b % 2 == 1) 
+          res = (res + a) % m; 
+
+      // Multiply 'a' with 2 
+      a = (a * 2) % m; 
+
+      // Divide b by 2 
+      b /= 2; 
+  } 
+
+  // Return result 
+  return res % m;
 }
