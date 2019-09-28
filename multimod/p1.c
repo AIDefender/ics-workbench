3266232244bd379620ce3509497ee802aeea692e
tracer-nju <tracer@nju.edu.cn>
2019-09-28 19:43:33 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 13dece63a2254db7b7051543685b3b9b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  19:43:33 up 1 day, 11:01,  1 user,  load average: 0.88, 0.87, 0.93

--- multimod/p1.c
+++ multimod/p1.c
@@ -12,7 +12,7 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   b1 = (int) b1;
   int32_t a2=(int) a;
   int32_t b2=(int) b;
-  m=(int) m;
+  m=(int32_t) m;
   printf("%d %d %d\n",a2,b2,m);
 
   return int_mod(a2,b2,m);
