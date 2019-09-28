e3933b54b162410346856881d19b6f79a2bbc8d6
tracer-nju <tracer@nju.edu.cn>
2019-09-28 21:20:10 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 13dece63a2254db7b7051543685b3b9b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  21:20:10 up 1 day, 12:38,  1 user,  load average: 0.75, 0.67, 0.90

--- multimod/p1.c
+++ multimod/p1.c
@@ -3,6 +3,12 @@
 
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-
+  uint32_t f = 0xffffffff;
+  uint32_t a2 = a&f;
+  uint32_t b2 = b&f;
+  int32_t a1 = a>>32 &f;
+  int32_t b1 = b>>32 &f;
+  printf("%d %d %u %u\n",a1,b1,a2,b2);
+  return 0;
 }
 
--- multimod/p2.c
+++ multimod/p2.c
@@ -1,6 +1,7 @@
 #include "multimod.h"
 
 int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
+  // ! 写完了TASK1才发现实现的其实是TASK2.就直接粘贴过来了.
   int64_t res=0;
   if (m==1) return 0;
   if (m==2) return (a&1)&&(b&1);
