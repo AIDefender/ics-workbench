cb5b52691f1571d163a9e9ce97849e01cf43e570
tracer-nju <tracer@nju.edu.cn>
2019-09-28 20:31:13 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 13dece63a2254db7b7051543685b3b9b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  20:31:14 up 1 day, 11:49,  1 user,  load average: 0.21, 0.72, 0.93

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,43 +1,24 @@
 #include "multimod.h"
 #include <stdio.h>
-#define ll int64_t
 
-int64_t int_mod(int64_t a, int64_t b, int64_t m);
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-
-  // int64_t f = 0xffffffff;
-
-  // int a1=a&18446744069414584320u >> 32;
-  // int b1=b&18446744069414584320u >> 32;
-  // printf("%d %d\n",a1,b1);
-  // int32_t a2=(int) a;
-  // int32_t b2=(int) b;
-
-  return int_mod(a,b,m);
-}
-
-int64_t int_mod(int64_t a, int64_t b, int64_t m){
-
   int64_t res=0;
   if (m==1) return 0;
   if (m==2) return (a&1)&&(b&1);
     
-  
   while (a > 0) 
   { 
-      // If a is odd, add 'a' to result 
-      if (a & 1) 
-          res = res % m + b % m; 
-
-      // Multiply 'b' with 2 
+      if (a & 1)
+      {
+        res = res % m + b % m; 
+      } 
+        
       b = (b % m) << 2; 
-
-      // Divide a ay 2 
       a >>= 1; 
   } 
-
-  // Return result 
   return res % m;
 
+
 }
+
