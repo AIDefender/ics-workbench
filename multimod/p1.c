39a28fe709789ecbb66b9a31fc2535ccb4946ab0
181220063-Xue Zhenghai <xuezh@smail.nju.edu.cn>
2019-09-28 20:32:02 +0800

    move code from task1 to task2

--- multimod/p1.c
+++ multimod/p1.c
@@ -3,22 +3,6 @@
 
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-  int64_t res=0;
-  if (m==1) return 0;
-  if (m==2) return (a&1)&&(b&1);
-    
-  while (a > 0) 
-  { 
-      if (a & 1)
-      {
-        res = res % m + b % m; 
-      } 
-        
-      b = (b % m) << 2; 
-      a >>= 1; 
-  } 
-  return res % m;
-
 
 }
 
--- multimod/p2.c
+++ multimod/p2.c
@@ -1,6 +1,21 @@
 #include "multimod.h"
 
 int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
-  // TODO: implement
-  return -2;
+  int64_t res=0;
+  if (m==1) return 0;
+  if (m==2) return (a&1)&&(b&1);
+    
+  while (a > 0) 
+  { 
+      if (a & 1)
+      {
+        res = res % m + b % m; 
+      } 
+        
+      b = (b % m) << 2; 
+      a >>= 1; 
+  } 
+  return res % m;
+
+
 }
