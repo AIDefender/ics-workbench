0e183c7cb0fd806cc961efb55a482737c729c6d8
tracer-nju <tracer@nju.edu.cn>
2019-09-28 20:26:05 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 13dece63a2254db7b7051543685b3b9b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  20:26:06 up 1 day, 11:44,  1 user,  load average: 0.32, 0.74, 0.97

--- multimod/p1.c
+++ multimod/p1.c
@@ -20,6 +20,8 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
 int64_t int_mod(int64_t a, int64_t b, int64_t m){
 
   int64_t res=0;
+  if (m==1) return 0;
+  if (m==2) return !(a&1)&&(b&1);
   
   while (a > 0) 
   { 
@@ -28,7 +30,7 @@ int64_t int_mod(int64_t a, int64_t b, int64_t m){
           res = res % m + b % m; 
 
       // Multiply 'b' with 2 
-      b = (b % m) * (2 % m); 
+      b = (b % m) << 2; 
 
       // Divide a ay 2 
       a >>= 1; 
