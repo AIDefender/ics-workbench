4f4b360fc551abe666df0875a7b6fff2bed8eff4
tracer-nju <tracer@nju.edu.cn>
2019-09-29 10:50:53 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: c967dbe1ac184f59b1ccf9aff794533b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  10:50:53 up  1:11,  1 user,  load average: 1.29, 1.49, 1.63

--- multimod/p2.c
+++ multimod/p2.c
@@ -1,7 +1,31 @@
 #include "multimod.h"
-
-int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
-  // ! 写完了TASK1才发现实现的其实是TASK2.就直接粘贴过来了.
+#include <stdio.h>
+#include <math.h> 
+#include <time.h>
+#include <stdlib.h>
+int64_t gen_rand_64();
+int64_t compute_res(int64_t,int64_t,int64_t);
+int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
+  time_t start, end;
+  time(&start);
+  int i;
+  for(i=0;i<100000;i++)
+  { 
+    a = gen_rand_64();
+    b = gen_rand_64();
+    m = gen_rand_64();
+    printf("%ld %ld %ld %ld\n",a,b,m,compute_res(a,b,m));
+  } 
+  time(&end);
+  printf("time=%ld\n",end-start); 
+  return 0;
+}
+int64_t gen_rand_64()
+{
+  return rand();
+}
+int64_t compute_res(int64_t a, int64_t b, int64_t m)
+{
   int64_t res=0;
   if (m==1) return 0;
   if (m==2) return (a&1)&&(b&1);
@@ -17,6 +41,5 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
       a >>= 1; 
   } 
   return res % m;
-
 
 }
