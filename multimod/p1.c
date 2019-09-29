bf09d61ae606b810b6f641bf96da2d06e0de7b84
tracer-nju <tracer@nju.edu.cn>
2019-09-29 11:41:40 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: c967dbe1ac184f59b1ccf9aff794533b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  11:41:40 up  2:01,  2 users,  load average: 0.88, 1.04, 1.26

--- multimod/p1.c
+++ multimod/p1.c
@@ -6,8 +6,7 @@
 static int64_t gen_rand_64();
 static int64_t compute_res(int64_t,int64_t,int64_t);
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-  time_t start, end;
-  time(&start);
+  clock_t start=clock(), diff;
   int i;
   for(i=0;i<100000;i++)
   { 
@@ -16,8 +15,9 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
     m = gen_rand_64();
     printf("%ld %ld %ld %ld\n",a,b,m,compute_res(a,b,m));
   } 
-  time(&end);
-  printf("time=%ld\n",end-start); 
+  diff = clock()-start;
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("time=%dms\n",msec); 
   return 0;
 }
 int64_t gen_rand_64()
