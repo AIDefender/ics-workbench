5999960e3a8ea30881da99ff92589b7edb0b2af5
tracer-nju <tracer@nju.edu.cn>
2019-09-30 07:54:39 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: c967dbe1ac184f59b1ccf9aff794533b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  07:54:39 up 22:14,  2 users,  load average: 0.81, 1.56, 2.29

--- multimod/p2.c
+++ multimod/p2.c
@@ -3,7 +3,7 @@
 #include <math.h> 
 #include <time.h>
 #include <stdlib.h>
-static int64_t gen_rand_64();
+// static int64_t gen_rand_64();
 static int64_t compute_res(int64_t,int64_t,int64_t);
 int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   // clock_t start=clock(), diff;
@@ -20,17 +20,17 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   // printf("time=%dms\n",msec); 
   return compute_res(a,b,m);
 }
-int64_t gen_rand_64()
-{
-  int i = 0;
-  int64_t res=0;
-  for(;i<61;i++)
-  {
-    res+=rand()%2;
-    res <<= 1;
-  }
-  return res;
-}
+// int64_t gen_rand_64()
+// {
+//   int i = 0;
+//   int64_t res=0;
+//   for(;i<61;i++)
+//   {
+//     res+=rand()%2;
+//     res <<= 1;
+//   }
+//   return res;
+// }
 int64_t compute_res(int64_t a, int64_t b, int64_t m)
 {
   int64_t res=0;
