5f97897967cc90bdb17d09326988a1d262af32aa
tracer-nju <tracer@nju.edu.cn>
2019-09-30 08:46:55 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: c967dbe1ac184f59b1ccf9aff794533b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  08:46:56 up 23:07,  2 users,  load average: 1.11, 1.39, 1.39

--- multimod/p1.c
+++ multimod/p1.c
@@ -47,6 +47,7 @@ int64_t compute_res(int64_t a, int64_t b, int64_t m)
     {
       int64_t am=a_bits[i]*(1<<i %m);
       int64_t bm=b_bits[j]*(1<<j %m);
+      printf("%ld %ld\n",am,bm);
       // int64_t bm=((int64_t)b_bits[j]<<j) %m;
       
       res+=(am*bm)%m;
