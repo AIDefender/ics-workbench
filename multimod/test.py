8fc5cb9b8b1f853381c050caf92aacc89a715c85
181220063-Xue Zhenghai <xuezh@smail.nju.edu.cn>
2019-09-30 08:03:34 +0800

    fix the <<2 bug(should be <<1) in task2. TASK2 TEST OK.

---
+++ multimod/test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+import os
+ops=[i.split() for i in os.popen("./multimod-64 -i 2 1 1 1")]
+for i in ops:
+    try:
+        i=[eval(j) for j in i]
+        if ((i[0]*i[1])%i[2])!=i[3]:
+            print(i)
+        else:
+            print("Good expression!")
+    except :
+        pass 
