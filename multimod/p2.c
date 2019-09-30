8cfd8a4d6f0b99e1d8c34adbf57fe938c9e347a7
tracer-nju <tracer@nju.edu.cn>
2019-09-30 08:02:07 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: c967dbe1ac184f59b1ccf9aff794533b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-64-generic       Architecture: x86-64  08:02:08 up 22:22,  2 users,  load average: 1.33, 1.07, 1.72

--- multimod/p2.c
+++ multimod/p2.c
@@ -3,34 +3,34 @@
 #include <math.h> 
 #include <time.h>
 #include <stdlib.h>
-// static int64_t gen_rand_64();
+static int64_t gen_rand_64();
 static int64_t compute_res(int64_t,int64_t,int64_t);
 int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
-  // clock_t start=clock(), diff;
-  // int i;
-  // for(i=0;i<1000;i++)
-  // { 
-  //   a = gen_rand_64();
-  //   b = gen_rand_64();
-  //   m = gen_rand_64();
-  //   printf("%ld %ld %ld %ld\n",a,b,m,compute_res(a,b,m));
-  // } 
-  // diff = clock()-start;
-  // int msec = diff * 1000 / CLOCKS_PER_SEC;
-  // printf("time=%dms\n",msec); 
+  clock_t start=clock(), diff;
+  int i;
+  for(i=0;i<1000;i++)
+  { 
+    a = gen_rand_64();
+    b = gen_rand_64();
+    m = gen_rand_64();
+    printf("%ld %ld %ld %ld\n",a,b,m,compute_res(a,b,m));
+  } 
+  diff = clock()-start;
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("time=%dms\n",msec); 
   return compute_res(a,b,m);
 }
-// int64_t gen_rand_64()
-// {
-//   int i = 0;
-//   int64_t res=0;
-//   for(;i<61;i++)
-//   {
-//     res+=rand()%2;
-//     res <<= 1;
-//   }
-//   return res;
-// }
+int64_t gen_rand_64()
+{
+  int i = 0;
+  int64_t res=0;
+  for(;i<61;i++)
+  {
+    res+=rand()%2;
+    res <<= 1;
+  }
+  return res;
+}
 int64_t compute_res(int64_t a, int64_t b, int64_t m)
 {
   int64_t res=0;
