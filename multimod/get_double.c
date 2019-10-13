e3951b08144af6d472d4a9d6d230efb6a5d8eea7
tracer-nju <tracer@nju.edu.cn>
2019-10-13 10:46:12 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 2d32a22b6bc24acb9e637ca4b2e1d869   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  10:46:13 up 1 day,  1:10,  1 user,  load average: 1.95, 1.66, 1.42

---
+++ multimod/get_double.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+int main(int argc, char** argv)
+{
+    assert(argc==2);
+    double x = atof(argv[1]);
+    printf("%.0f\n",x);
+}
--- multimod/p3.c
+++ multimod/p3.c
@@ -2,5 +2,9 @@
 
 int64_t multimod_p3(int64_t a, int64_t b, int64_t m) {
   int64_t t = (a * b - (int64_t)((double)a * b / m) * m) % m;
-  return t < 0 ? t + m : t;
+  while (t>=0)
+  {
+    t+=m;
+  }
+  return t;
 }
