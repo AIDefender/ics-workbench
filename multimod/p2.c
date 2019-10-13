2ff44c9a00fac3d1b8a4057770849df29f78750e
tracer-nju <tracer@nju.edu.cn>
2019-10-13 08:06:39 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 2d32a22b6bc24acb9e637ca4b2e1d869   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  08:06:39 up 22:30,  1 user,  load average: 0.80, 0.84, 0.93

--- multimod/p2.c
+++ multimod/p2.c
@@ -18,7 +18,7 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
     m = gen_rand_64();
     assert(m>0);
     // assert(b%m>0);
-    printf("%x %x\n",a%m,b%m);
+    printf("%ld %ld\n",a%m,b%m);
     printf("%ld %ld %ld\n",a,b,m);
     printf("%ld\n",compute_res(a,b,m));
   } 
