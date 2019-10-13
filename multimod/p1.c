2c32b9e1fda8dac41e79d6b64c9d88577c1cb77a
tracer-nju <tracer@nju.edu.cn>
2019-10-13 08:49:31 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 2d32a22b6bc24acb9e637ca4b2e1d869   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  08:49:31 up 23:13,  1 user,  load average: 0.85, 0.53, 0.66

--- multimod/p1.c
+++ multimod/p1.c
@@ -80,7 +80,7 @@ int64_t compute_res(int64_t a, int64_t b, int64_t m)
       // {
       //   am = (am << 1)%m;
       // }
-      res = res%m+am%m;
+      res = (res%m+am%m)%m;
       // assert(res>>63==0);
     }
   }
