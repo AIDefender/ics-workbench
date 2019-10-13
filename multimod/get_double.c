351b37c4264dcae9a513e06469be29953c1ffe2c
tracer-nju <tracer@nju.edu.cn>
2019-10-13 10:46:48 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 2d32a22b6bc24acb9e637ca4b2e1d869   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  10:46:48 up 1 day,  1:11,  1 user,  load average: 1.80, 1.65, 1.42

--- multimod/get_double.c
+++ multimod/get_double.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
-static int main(int argc, char** argv)
-{
-    assert(argc==2);
-    double x = atof(argv[1]);
-    printf("%.0f\n",x);
+// int main(int argc, char** argv)
+// {
+//     assert(argc==2);
+//     double x = atof(argv[1]);
+//     printf("%.0f\n",x);
 }
