e447b581d5b118e8c9f6d37b4c4d10d423dfadcb
tracer-nju <tracer@nju.edu.cn>
2019-10-28 20:04:33 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 74776ae57f7d45fc988712c5c6bbb56f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  20:04:33 up 20:53,  1 user,  load average: 0.73, 0.98, 1.01

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -25,8 +25,8 @@ int asm_popcnt(uint64_t n) {
        "incl -0x4(%%rbp)\n"
        "cmp $0x3f,-0x4(%%rbp)\n"
        "je dest2\n"
-       "mov %%eax, -0x18(%%ebp)\n"
-       "mov -0x18(%%ebp), %[out]\n"
+       "mov %%rax, -0x18(%%rbp)\n"
+       "mov -0x18(%%rbp), %[out]\n"
        : [out] "+g"(x)
        : [in] "r" (n)
        : "%rax","cc","memory");
