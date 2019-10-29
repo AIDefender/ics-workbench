efd3eff7bd2b45d557123022b95323e987e6c760
tracer-nju <tracer@nju.edu.cn>
2019-10-29 08:54:27 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 74776ae57f7d45fc988712c5c6bbb56f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  08:54:27 up 1 day,  9:42,  1 user,  load average: 2.46, 1.74, 1.57

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -54,10 +54,10 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
        "addq $0x1, -0x18(%%rbp)\n"
        "addq $0x1, -0x20(%%rbp)\n"
        "addq $0x1, -0xc(%%rbp)\n"
-       "start: mov -0xc(%%rbp),%%rax\n"bb
+       "start: mov -0xc(%%rbp),%%rax\n"
        "cltq\n"
        "cmp %%rax, -0x28(%%rbp)\n"
-       "ja loop\n"b
+       "ja loop\n"
        : [pos] "+g"(pos)
        :  [dest] "m" (dest), [src] "m"(src), [n] "m"(n)
        : "%rax", "%edx", "cc", "memory");
