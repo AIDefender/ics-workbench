55ad02d880d469cf6b0e0cf75a8cd081cb69744a
tracer-nju <tracer@nju.edu.cn>
2019-10-29 16:04:10 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 74776ae57f7d45fc988712c5c6bbb56f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  16:04:11 up 1 day, 16:52,  1 user,  load average: 1.61, 1.23, 1.01

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -65,7 +65,19 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-  // TODO: implement
+  asm ("mov %%rbx, (%[env])\n" // * 保存rbx
+       "mov (%%rsp), %%rax\n"
+       "mov %%rax, 0x8(%[env])\n" // * rsp存放rbp的旧址
+       "mov %%r12, 0x10(%[env])\n"
+       "mov %%r13, 0x18(%[env])\n"
+       "mov %%r14, 0x20(%[env])\n"
+       "mov %%r15, 0x28(%[env])\n"
+       "lea 0x10(%%rsp), %%rax\n"
+       "mov %%rax, 0x30(%[env])\n" // * rsp+10的地址是rsp的旧值
+       "mov 0x8(%%rsp), %%rax\n"
+       "mov %%rax, 0x38(%[env])\n"  // * rsp+8存放pc
+       : [env] "+g"(env)
+       : "cc", "memory");
   return 0;
 }
 
--- asm/asm.h
+++ asm/asm.h
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 // TODO: set correct array length
-typedef uintptr_t asm_jmp_buf[0];
+typedef uintptr_t asm_jmp_buf[8]; //*里面都是指针类型
 
 int64_t asm_add(int64_t a, int64_t b);
 int asm_popcnt(uint64_t x);
