f7aa5efe77e68cfdfa4499b84e431b89456d5835
tracer-nju <tracer@nju.edu.cn>
2019-10-29 16:17:47 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7b0e8a90ef82450581b42b4b738412b6            Boot ID: 74776ae57f7d45fc988712c5c6bbb56f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-65-generic       Architecture: x86-64  16:17:47 up 1 day, 17:06,  1 user,  load average: 0.27, 0.65, 0.86

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -77,7 +77,7 @@ int asm_setjmp(asm_jmp_buf env) {
        "mov 0x8(%%rsp), %%rax\n"
        "mov %%rax, 0x38%[env]\n"  // * rsp+8存放pc
        : 
-       : [env] "r"(env)
+       : [env] "g"(env)
        : "%rax", "cc", "memory");
   return 0;
 }
