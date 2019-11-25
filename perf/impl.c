009120e09523503a28237eceb7bda7445cd4cb76
tracer-nju <tracer@nju.edu.cn>
2019-11-25 20:01:04 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: 68d1578535104bdda42f07551bbbeb0f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  20:01:05 up  4:25,  1 user,  load average: 1.54, 1.46, 1.43

--- perf/impl.c
+++ perf/impl.c
@@ -1,7 +1,7 @@
 #include "perf.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 void dummy() {
 }
 
@@ -19,3 +19,67 @@ void simple_loop() {
     // printf("haha");
 }
 
+int64_t gen_rand_64()
+{
+  //1
+  int i = 0;
+  int64_t res=rand()%2;
+  for(;i<62;i++)
+  {
+    res <<= 1;
+    res+=rand()%2;
+  }
+  return res;
+}
+
+void multimod_p1() {
+
+  a = gen_rand_64();
+  b = gen_rand_64();
+  m = gen_rand_64();
+
+  volatile int64_t res = compute_res_1(a,b,m);
+}
+
+int64_t compute_res_1(int64_t a, int64_t b, int64_t m)
+{
+  int *a_bits = gen_bits(a);
+  int *b_bits = gen_bits(b);
+  int i = 0;
+  int j = 0;
+  uint64_t res=0;
+  for(i=0;i<63;i++)
+  {
+    for(j=0;j<63;j++)
+    {
+
+      if (b_bits[j]==0)
+      {
+        continue;
+      }
+      int k;
+      int64_t am=a_bits[i];
+      for(k=0;k<i+j;k++)
+      {
+        if (am>m)
+        {
+          am = (am % m) << 1; 
+        }  
+        else 
+        {
+          if (am>(m>>1))
+          {
+            am=(am-m+am)%m;
+          }
+          else 
+          {
+            am<<=1;
+          }
+        }
+      }
+      res = (res%m+am%m)%m;
+    }
+  }
+  
+  return (int64_t)res;
+}
--- perf/perf.h
+++ perf/perf.h
@@ -2,6 +2,7 @@
   _(dummy) \
   _(print_hello) \
   _(print_error) \
-  _(simple_loop)
+  _(simple_loop) \
+  _(multimod_p1)
 
 #define LENGTH(arr) (sizeof(arr) / sizeof(arr[0]))
