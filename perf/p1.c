a35dc382c643c8137cce6d2fb4b1e420600c31e9
tracer-nju <tracer@nju.edu.cn>
2019-11-25 20:08:12 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: 68d1578535104bdda42f07551bbbeb0f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  20:08:13 up  4:33,  1 user,  load average: 2.96, 2.07, 1.68

--- perf/p1.c
+++ perf/p1.c
@@ -15,7 +15,8 @@ void multimod_p1() {
   int64_t b = gen_rand_64();
   int64_t m = gen_rand_64();
 
-  static volatile int64_t res __attribute__((used)) = compute_res(a,b,m);
+  static volatile int64_t res __attribute__((used)) = 0;
+  res = compute_res(a,b,m);
 
 }
 int64_t gen_rand_64()
