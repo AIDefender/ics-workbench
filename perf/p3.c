33ec229e4aaee6c0ac8a0e446e196b61b2c306b6
tracer-nju <tracer@nju.edu.cn>
2019-11-26 09:22:00 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: cfe3bef239614239aa3bfb080b33a085   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  09:22:00 up 10:08,  1 user,  load average: 1.23, 0.85, 0.46

---
+++ perf/p3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h> 
+#include <time.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+static int64_t gen_rand_64();
+static int64_t compute_res(int64_t,int64_t,int64_t);
+
+void multimod_p3(void) {
+
+  int64_t a = gen_rand_64();
+  int64_t b = gen_rand_64();
+  int64_t m = gen_rand_64();
+
+  static volatile int64_t res __attribute__((used)) = 0;
+  res = compute_res(a,b,m);
+
+}
+
+
+int64_t compute_res(int64_t a, int64_t b, int64_t m) {
+  int64_t t = (a * b - (int64_t)((double)a * b / m) * m) % m;
+  return t < 0 ? t + m : t;
+}
+int64_t gen_rand_64()
+{
+  int i = 0;
+  int64_t res=rand()%2;
+  for(;i<62;i++)
+  {
+    res <<= 1;
+    res+=rand()%2;
+  }
+  return res;
+}
--- perf/perf.h
+++ perf/perf.h
@@ -4,6 +4,7 @@
   _(print_error) \
   _(simple_loop) \
   _(multimod_p1) \
-  _(multimod_p2)
+  _(multimod_p2) \
+  _(multimod_p3) \
 
 #define LENGTH(arr) (sizeof(arr) / sizeof(arr[0]))
