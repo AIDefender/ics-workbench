c24014e071920ce157f8e7c111d946b77ddc6dd2
tracer-nju <tracer@nju.edu.cn>
2019-11-26 16:12:53 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: b4801df6e39e4ff2b158088ce1c74879   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  16:12:53 up 27 min,  1 user,  load average: 0.59, 0.99, 1.07

--- perf/util.c
+++ perf/util.c
@@ -1,17 +1,24 @@
 #include <stdlib.h>
 #include "util.h"
 #ifdef WRITE_FILE
-int64_t gen_rand_64(int* num_1)
+int64_t gen_rand_64(int* num_1, int* hi_1)
 {
   int i = 0;
   *num_1 = 0;
+  *hi_1 = 0;
   int64_t res=rand()%2;
-  if (res) (*num_1)++;
+  if (res) 
+  {
+    (*num_1)++;
+  }
   for(;i<62;i++)
   {
     res <<= 1;
     int num_gen = rand()%2;
-    if (num_gen) (*num_1)++;
+    if (num_gen){
+      (*num_1)++;
+      (*hi_1) = i+1;
+    } 
     res+=num_gen;
   }
   return res;
--- perf/util.h
+++ perf/util.h
@@ -9,16 +9,22 @@
 #define GEN_FUNC(x) \
     void multimod_p##x(void) { \
         int a_num_1 = 0; \
+        int a_hi_1 = 0; \
         int b_num_1 = 0; \
+        int b_hi_1 = 0; \
         int m_num_1 = 0; \
-        int64_t a = gen_rand_64(&a_num_1); \
-        int64_t b = gen_rand_64(&b_num_1); \
-        int64_t m = gen_rand_64(&m_num_1); \
+        int m_hi_1 = 0; \
+        int64_t a = gen_rand_64(&a_num_1,&a_hi_1); \
+        int64_t b = gen_rand_64(&b_num_1,&b_hi_1); \
+        int64_t m = gen_rand_64(&m_num_1,&c_hi_1); \
         static volatile int64_t res __attribute__((used)) = 0; \
         res = compute_res(a,b,m); \
         FILE* file; \
         file = fopen("num_1","a"); \
         fprintf(file,"%d,%d,%d\n",a_num_1,b_num_1,m_num_1); \
+        fclose(file); \
+        file = fopen("hi_1","a"); \
+        fprintf(file,"%d,%d,%d\n",a_hi_1,b_hi_1,m_hi_1); \
         fclose(file); \
     }
 
