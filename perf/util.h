d9608876cecf716f1e1963cc27c0b09ed65f4556
tracer-nju <tracer@nju.edu.cn>
2019-11-26 16:13:33 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: b4801df6e39e4ff2b158088ce1c74879   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  16:13:33 up 28 min,  1 user,  load average: 1.03, 1.07, 1.09

--- perf/util.h
+++ perf/util.h
@@ -16,7 +16,7 @@
         int m_hi_1 = 0; \
         int64_t a = gen_rand_64(&a_num_1,&a_hi_1); \
         int64_t b = gen_rand_64(&b_num_1,&b_hi_1); \
-        int64_t m = gen_rand_64(&m_num_1,&c_hi_1); \
+        int64_t m = gen_rand_64(&m_num_1,&m_hi_1); \
         static volatile int64_t res __attribute__((used)) = 0; \
         res = compute_res(a,b,m); \
         FILE* file; \
