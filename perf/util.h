10bf141a26ab37e24fb912c9473fa956cb75c9b7
tracer-nju <tracer@nju.edu.cn>
2019-11-26 09:53:56 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: cfe3bef239614239aa3bfb080b33a085   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  09:53:56 up 10:40,  1 user,  load average: 0.57, 0.92, 0.84

--- perf/util.h
+++ perf/util.h
@@ -1,6 +1,28 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 #include <stdint.h>
+#include <stdio.h>
+
+#define WRITE_FILE
+
+#ifdef WRITE_FILE
+#define GEN_FUNC(x) \
+    void multimod_p##x(void) { \
+        int a_num_1 = 0; \
+        int b_num_1 = 0; \
+        int m_num_1 = 0; \
+        int64_t a = gen_rand_64(&a_num_1); \
+        int64_t b = gen_rand_64(&b_num_1); \
+        int64_t m = gen_rand_64(&m_num_1); \
+        static volatile int64_t res __attribute__((used)) = 0; \
+        res = compute_res(a,b,m); \
+        FILE* file; \
+        file = fopen("num_1","w+"); \
+        fprintf(file,"%d,%d,%d",a_num_1,b_num_1,c_num_1); \
+    }
+    
+int64_t gen_rand_64(int*);
+#else
 #define GEN_FUNC(x) \
     void multimod_p##x(void) { \
         int64_t a = gen_rand_64(); \
@@ -9,7 +31,6 @@
         static volatile int64_t res __attribute__((used)) = 0; \
         res = compute_res(a,b,m); \
     }
-
 int64_t gen_rand_64();
-
+#endif
 #endif
