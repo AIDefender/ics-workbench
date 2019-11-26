2a39e6954bd9e389cdecc5fc011ed3ccb9ff17ce
tracer-nju <tracer@nju.edu.cn>
2019-11-26 09:56:17 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: cfe3bef239614239aa3bfb080b33a085   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-70-generic       Architecture: x86-64  09:56:17 up 10:42,  1 user,  load average: 0.93, 1.05, 0.91

--- perf/util.c
+++ perf/util.c
@@ -4,14 +4,14 @@
 int64_t gen_rand_64(int* num_1)
 {
   int i = 0;
-  num_1 = 0;
+  *num_1 = 0;
   int64_t res=rand()%2;
-  if (res) num_1++;
+  if (res) *num_1++;
   for(;i<62;i++)
   {
     res <<= 1;
     int num_gen = rand()%2;
-    if (num_gen) num_1++;
+    if (num_gen) *num_1++;
     res+=num_gen;
   }
   return res;
