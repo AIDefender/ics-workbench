b0da52fe883d3542ec8f7c1acd2a60cd969a5da6
tracer-nju <tracer@nju.edu.cn>
2019-12-06 18:23:45 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: 97b4d9d36c61445b8b0c5d81a69e9bf6   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 4.15.0-72-generic       Architecture: x86-64  18:23:46 up  8:07,  1 user,  load average: 0.75, 0.58, 0.50

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -29,7 +29,6 @@ void print_bi(uint32_t num, int width)
 }
 void show_cache()
 {
-  return;
   int i = 0;
   for (;i < row_cache; i++)
   {
@@ -104,7 +103,7 @@ void cycle_increase(int n) { cycle_cnt += n; }
 // TODO: implement the following functions
 
 uint32_t cache_read(uintptr_t addr) {
-  show_cache(); 
+  // show_cache(); 
   bool success = false;
   uint32_t ret = query_cache_hit(addr,&success);
   
