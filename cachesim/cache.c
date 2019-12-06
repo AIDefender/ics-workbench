72491d45b5b1c1a62801919901742a40690763bf
tracer-nju <tracer@nju.edu.cn>
2019-12-06 21:55:07 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: 97b4d9d36c61445b8b0c5d81a69e9bf6   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 4.15.0-72-generic       Architecture: x86-64  21:55:07 up 11:38,  1 user,  load average: 0.87, 0.64, 0.54

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -87,5 +87,5 @@ void init_cache(int total_size_width, int associativity_width) {
 }
 
 void display_statistic(void) {
-  printf("hit num:%u, miss num:%u, hit rate:%f%%\n.",hit_num,miss_num,(double)hit_num/miss_num);
+  printf("hit num:%u, miss num:%u, hit rate:%f%%\n.",hit_num,miss_num,(double)hit_num/(hit_num+miss_num));
 }
