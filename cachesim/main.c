de657261bc3c5f7e54088cb2b8edb3664a1f3e28
tracer-nju <tracer@nju.edu.cn>
2019-12-06 22:12:07 +0800

    Static hostname: xzh          Icon name: computer-laptop            Chassis: laptop         Machine ID: a325904529bc4f1a8d8459199a496c97            Boot ID: 97b4d9d36c61445b8b0c5d81a69e9bf6   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 4.15.0-72-generic       Architecture: x86-64  22:12:07 up 11:55,  1 user,  load average: 0.18, 0.61, 0.63

--- cachesim/main.c
+++ cachesim/main.c
@@ -90,8 +90,8 @@ static void parse_args(int argc, char *argv[]) {
                   has_seed = true;
                 }
                 break;
-      case 't': tot_sz_width = strtol(optarg,&p,0);
-      case 'a': asso_width = strtol(optarg,&p,0);
+      case 't': tot_sz_width = atoi(optarg);
+      case 'a': asso_width = atoi(optarg);
       case 1:
                 if (tracefile != NULL) printf("too much argument '%s', ignored\n", optarg);
                 else tracefile = optarg;
